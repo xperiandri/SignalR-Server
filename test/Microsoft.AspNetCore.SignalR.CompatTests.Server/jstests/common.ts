62f02bbd66edf95ddbfd6dafd7134c1fff14e86b
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-09-06 15:01:37 -0700

    add JavaScript browser-based compat tests
    Note: nothing runs them in the build at the moment, but they all pass
    when launched in the browser. Next phase will be automated command-line
    based running.

---
+++ test/Microsoft.AspNetCore.SignalR.CompatTests.Server/jstests/common.ts
@@ -0,0 +1,11 @@
+﻿const HUBS_URL = '/test/hubs';
+const CONNECTION_URL = '/test/raw';
+
+// Some tests can take 15secs :(.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+
+function eachTransport(f: (transport: SignalR.Transport) => void) {
+    var transportNames = Object.keys($.signalR.transports)
+        .filter(x => x != "foreverFrame" && x.length > 0 && x[0] != '_');
+    transportNames.forEach(t => f($.signalR.transports[t] as SignalR.Transport));
+}
